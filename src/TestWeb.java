695a198374a0df67516482bfccdc26a254274f7e
tiandiyihao0 <654969283@qq.com>
2018-07-26 19:49:02 +0800

    项目提交

---
+++ src/TestWeb.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Administrator
+ * Date: 2018-07-26
+ * Time: 18:58
+ */
+public class TestWeb {
+    public static void main(String[] args) {
+        int num = 10;
+        int str = 11;
+        System.out.println(str);
+        System.out.println("num = " + num);
+    }
+}
